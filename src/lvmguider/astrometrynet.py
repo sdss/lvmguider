03d0d9e7f509ad72972f5a4c9b2bf6019bb9f005
José Sánchez-Gallego <gallegoj@uw.edu>
2023-06-18 07:01:56 +0000

    Add astrometrynet and transformations

---
+++ src/lvmguider/astrometrynet.py
@@ -0,0 +1,461 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2023-06-18
+# @Filename: astrometrynet.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+from __future__ import annotations
+
+import asyncio
+import os
+import pathlib
+import re
+import subprocess
+import tempfile
+import time
+import warnings
+from glob import glob
+
+from typing import NamedTuple, Optional, TypeVar
+
+import numpy
+import pandas
+from astropy.table import Table
+from astropy.wcs import WCS
+
+
+PathLike = TypeVar("PathLike", pathlib.Path, str)
+
+
+class TimedProcess(NamedTuple):
+    """A completed process which includes its elapsed time."""
+
+    process: asyncio.subprocess.Process | subprocess.CompletedProcess
+    elapsed: float
+
+
+class AstrometryNet:
+    """A wrapper for the astrometry.net ``solve-field`` command.
+
+    Parameters
+    ----------
+    configure_params
+        Parameters to be passed to `.configure`.
+    """
+
+    def __init__(self, **configure_params):
+        solve_field_cmd = subprocess.run(
+            "which solve-field",
+            shell=True,
+            capture_output=True,
+        )
+        solve_field_cmd.check_returncode()
+
+        self.solve_field_cmd = solve_field_cmd.stdout.decode().strip()
+
+        self._options = {}
+        self.configure(**configure_params)
+
+    def configure(
+        self,
+        backend_config: Optional[str] = None,
+        width: Optional[int] = None,
+        height: Optional[int] = None,
+        sort_column: Optional[str] = None,
+        sort_ascending: Optional[bool] = None,
+        no_plots: Optional[bool] = None,
+        ra: Optional[float] = None,
+        dec: Optional[float] = None,
+        radius: Optional[float] = None,
+        scale_low: Optional[float] = None,
+        scale_high: Optional[float] = None,
+        scale_units: Optional[str] = None,
+        dir: Optional[str] = None,
+        **kwargs,
+    ):
+        """Configures how to run of ``solve-field```.
+
+        The parameters this method accepts are identical to those of
+        ``solve-field`` and are passed unchanged.
+
+        Parameters
+        ----------
+        backend_config
+            Use this config file for the ``astrometry-engine`` program.
+        width
+            Specify the field width, in pixels.
+        height
+            Specify the field height, in pixels.
+        sort_column
+            The FITS column that should be used to sort the sources.
+        sort_ascending
+            Sort in ascending order (smallest first);
+            default is descending order.
+        no_plot
+            Do not produce plots.
+        ra
+            RA of field center for search, in degrees.
+        dec
+            Dec of field center for search, in degrees.
+        radius
+            Only search in indexes within ``radius`` degrees of the field
+            center given by ``ra`` and ``dec``.
+        scale_low
+            Lower bound of image scale estimate.
+        scale_high
+            Upper bound of image scale estimate.
+        scale_units
+            In what units are the lower and upper bounds? Choices:
+            ``'degwidth'``, ``'arcminwidth'``, ``'arcsecperpix'``,
+            ``'focalmm'``.
+        dir
+            Path to the directory where all output files will be saved.
+        """
+
+        self._options = {
+            "backend-config": backend_config,
+            "width": width,
+            "height": height,
+            "sort-column": sort_column,
+            "sort-ascending": sort_ascending,
+            "no-plots": no_plots,
+            "ra": ra,
+            "dec": dec,
+            "radius": radius,
+            "scale-low": scale_low,
+            "scale-high": scale_high,
+            "scale-units": scale_units,
+            "dir": dir,
+            "overwrite": True,
+        }
+        self._options.update(self._format_options(kwargs))
+
+        return
+
+    def _format_options(self, options: dict):
+        """Formats the option names."""
+
+        formatted_options = {}
+
+        for key, value in options.items():
+            formatted_key = key.replace("_", "-")
+            formatted_options[formatted_key] = value
+
+        return formatted_options
+
+    def _build_command(self, files, options=None):
+        """Builds the ``solve-field`` command to run."""
+
+        if options is None:
+            options = self._options
+
+        flags = ["no-plots", "sort-ascending", "overwrite"]
+
+        cmd = [self.solve_field_cmd]
+
+        for option in options:
+            if options[option] is None:
+                continue
+            if option in flags:
+                if options[option] is True:
+                    cmd.append("--" + option)
+            else:
+                cmd.append("--" + option)
+                cmd.append(str(options[option]))
+
+        cmd += list(files)
+
+        return cmd
+
+    def run_sync(
+        self,
+        files: PathLike | list[PathLike],
+        shell: bool = True,
+        stdout: Optional[PathLike] = None,
+        stderr: Optional[PathLike] = None,
+        **kwargs,
+    ):
+        """Runs astrometry.net synchronously.
+
+        Parameters
+        ----------
+        files
+            List of files to be processed.
+        shell
+            Whether to call `subprocess.run` with ``shell=True``.
+        stdout
+            Path where to save the stdout output.
+        stderr
+            Path where to save the stderr output.
+        kwargs
+            Configuration parameters (see `.configure`) to override. The
+            configuration applies only to this run of ``solve-field`` and it
+            is not saved.
+
+        Returns
+        -------
+        `subprocess.CompletedProcess`
+            The completed process.
+
+        """
+
+        options = self._options.copy()
+        options.update(self._format_options(kwargs))
+
+        if not isinstance(files, (tuple, list)):
+            files = [files]
+
+        t0 = time.time()
+
+        args = self._build_command(list(map(str, files)), options=options)
+        if shell:
+            args = " ".join(args)
+
+        cmd = subprocess.run(
+            args,
+            shell=shell,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+
+        elapsed = time.time() - t0
+
+        if stdout:
+            with open(stdout, "wb") as out:
+                if isinstance(args, str):
+                    out.write(args.encode() + b"\n")
+                else:
+                    out.write(" ".join(args).encode() + b"\n")
+                out.write(cmd.stdout)
+
+        if stderr:
+            with open(stderr, "wb") as err:
+                err.write(cmd.stderr)
+
+        return TimedProcess(cmd, elapsed)
+
+    async def run_async(
+        self,
+        files: PathLike | list[PathLike],
+        shell: bool = True,
+        stdout: Optional[PathLike] = None,
+        stderr: Optional[PathLike] = None,
+        **kwargs,
+    ) -> TimedProcess:
+        """Runs astrometry.net asynchronously.
+
+        Parameters
+        ----------
+        files
+            List of files to be processed.
+        shell
+            Whether to call `subprocess.run` with ``shell=True``.
+        stdout
+            Path where to save the stdout output.
+        stderr
+            Path where to save the stderr output.
+        kwargs
+            Configuration parameters (see `.configure`) to override. The
+            configuration applies only to this run of ``solve-field`` and it
+            is not saved.
+
+        Returns
+        -------
+        `subprocess.CompletedProcess`
+            The completed process.
+
+        """
+
+        options = self._options.copy()
+        options.update(self._format_options(kwargs))
+
+        if not isinstance(files, (tuple, list)):
+            files = [files]
+
+        t0 = time.time()
+
+        args = self._build_command(list(map(str, files)), options=options)
+
+        if shell:
+            cmd = await asyncio.create_subprocess_shell(
+                " ".join(args),
+                stdout=asyncio.subprocess.PIPE,
+                stderr=asyncio.subprocess.PIPE,
+            )
+
+        else:
+            cmd = await asyncio.create_subprocess_exec(
+                *args,
+                stdout=asyncio.subprocess.PIPE,
+                stderr=asyncio.subprocess.PIPE,
+            )
+
+        stdout_bytes, stderr_bytes = await cmd.communicate()
+
+        elapsed = time.time() - t0
+
+        if stdout:
+            with open(stdout, "wb") as out:
+                if isinstance(args, str):
+                    out.write(args.encode() + b"\n")
+                else:
+                    out.write(" ".join(args).encode() + b"\n")
+                out.write(stdout_bytes)
+
+        if stderr:
+            with open(stderr, "wb") as err:
+                err.write(stderr_bytes)
+
+        return TimedProcess(cmd, elapsed)
+
+
+def astrometrynet_quick(
+    index_path: str,
+    regions: pandas.DataFrame | Table | numpy.ndarray,
+    ra: float,
+    dec: float,
+    pixel_scale: float,
+    pixel_scale_factor_hi: float = 1.1,
+    pixel_scale_factor_lo: float = 0.9,
+    scales: int | list[int] | None = None,
+    radius: float = 0.5,
+    width: float = 2048,
+    height: float = 2048,
+    series: int | None = None,
+    verbose: bool = False,
+    **kwargs,
+):
+    """Quickly process a set of detections using astrometry.net.
+
+    Parameters
+    ----------
+    index_path
+        The path to the index files to use.
+    regions
+        A pandas data frame of source detections with at least three columns:
+        ``x``, ``y``, and ``flux``. ``flux`` can be set to all ones if the
+        fluxes have not been measured.
+    ra
+        The estimated RA of the field, in degrees.
+    dec
+        The estimated declination of the field, in degrees.
+    pixel_scale
+        The estimated pixel scale, in arcsec.
+    pixel_scale_factor_lo
+        A multiplicative factor for the pixel scale to calculate the
+        lowest possible pixel scale to attempt.
+    pixel_scale_factor_hi
+        A multiplicative factor for the pixel scale to calculate the
+        highest possible pixel scale to attempt.
+    scales
+        Index files scales to use. Otherwise uses all index files.
+    radius
+        The radius, in degrees, around ``ra`` and ``dec`` to search.
+    width
+        The width of the image in pixels.
+    height
+        The height of the image in pixels.
+    series
+        The index series. If not provided and needed, will try to determine from
+        ``index_path``.
+    verbose
+        Raise an error if the field is not solved.
+    kwargs
+        Other options to pass to `.AstrometryNet`.
+
+    Returns
+    -------
+    wcs
+        An astropy WCS with the solved field, or `None` if the field could
+        not be solved.
+
+    """
+
+    if not isinstance(regions, Table):
+        regions = pandas.DataFrame(regions)
+        xyls = Table.from_pandas(regions)
+    else:
+        xyls = regions
+
+    outtempfile = tempfile.NamedTemporaryFile().name
+
+    index_path = os.path.abspath(index_path)
+    index_files = glob("index-*.fit*", root_dir=index_path)
+
+    if len(index_files) == 0:
+        raise ValueError(f"No index files found in {index_path}.")
+
+    backend_config = os.path.join(os.path.dirname(outtempfile), "astrometrynet.cfg")
+    with open(backend_config, "w") as ff:
+        ff.write(
+            f"""inparallel
+cpulimit 30
+add_path {index_path}
+"""
+        )
+
+        if scales is None:
+            ff.write("autoindex\n")
+        else:
+            if series is None:
+                match = re.match(r"index\-([0-9]+)\-", str(index_files[0]))
+                if match:
+                    series = int(int(match.group(1)) / 100) * 100
+                else:
+                    raise ValueError("Index series cannot be determined.")
+
+            if isinstance(scales, int):
+                scales = [scales]
+            for scale in scales:
+                series_scale = series + scale
+                scale_files = glob(f"index-{series_scale}-*.fits", root_dir=index_path)
+                for scale_file in scale_files:
+                    ff.write(f"index {scale_file}\n")
+
+    opts = dict(
+        backend_config=str(backend_config),
+        width=width,
+        height=height,
+        no_plots=True,
+        scale_low=pixel_scale * pixel_scale_factor_lo,
+        scale_high=pixel_scale * pixel_scale_factor_hi,
+        scale_units="arcsecperpix",
+        sort_column="flux",
+        radius=radius,
+    )
+    opts.update(kwargs)
+
+    astrometry = AstrometryNet(**opts)
+
+    xyls_path = outtempfile + ".xyls"
+    xyls.write(xyls_path, format="fits", overwrite=True)
+
+    wcs_output = pathlib.Path(outtempfile + ".wcs")
+    wcs_output.unlink(missing_ok=True)
+
+    astrometry.run_sync(
+        [xyls_path],
+        stdout=outtempfile + ".stdout",
+        stderr=outtempfile + ".stderr",
+        ra=ra,
+        dec=dec,
+    )
+
+    if wcs_output.exists():
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            wcs = WCS(open(wcs_output).read())
+        wcs_output.unlink()
+        return wcs
+    else:
+        if verbose:
+            if os.path.exists(outtempfile + ".stdout"):
+                stdout = open(outtempfile + ".stdout").read()
+                stderr = open(outtempfile + ".stderr").read()
+                raise RuntimeError(stdout + "\n" + stderr)
+            else:
+                raise RuntimeError("Unexpected error. No stderr was generated.")
+
+    return None
---
+++ src/lvmguider/transformations.py
@@ -0,0 +1,230 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2023-06-18
+# @Filename: transformations.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+import numpy
+import pandas
+from astropy.io import fits
+from astropy.wcs.utils import pixel_to_skycoord
+
+from lvmguider.astrometrynet import astrometrynet_quick
+
+
+def rot_shift_locs(
+    camera: str,
+    in_locs: numpy.ndarray,
+    rot_shift: tuple | None = None,
+    verbose=False,
+):
+    """Rotate and shift the star locations from an AG frame to the correct locations in
+    the large "master frame" corresponding to the full focal plane.
+
+    Parameters
+    ----------
+    camera:
+        Which AG camera: ``skyw-e``, ``skyw-w``, ``sci-c``, ``sci-e``, ``sci-w``,
+        ``skye-e``, ``skye-w``, or ``spec-e``.
+    in_locs
+        Numpy 2D array of (x, y) star locations.
+    rot_shift
+        List of input ``[theta_deg, Sx, Sz]` to use. If `None`, use best fit values
+
+    Returns
+    -------
+    rs_loc
+        Array of corresponding star locations in "master frame".
+
+    """
+
+    # Get best-fit rotation and shift, based on NonLinLSQ_Fit.py
+
+    if rot_shift is None:  # Use Best-Fit values from Suff_AGC.docx
+        cam_idx = [
+            "skyw-w",
+            "skyw-e",
+            "sci-w",
+            "sci-e",
+            "spec-e",
+            "skye-w",
+            "skye-e",
+            "sci-c",
+        ].index(camera)
+
+        # Best-fit rotations and shifts
+        bF = numpy.array(
+            (
+                [
+                    [-89.85, -76.53, 430.64],
+                    [90.16, 3512.90, 433.74],
+                    [-89.84, -28.26, 415.33],
+                    [89.98, 3512.74, 447.25],
+                    [89.60, 3193.11, 504.26],
+                    [-90.69, -79.54, 436.38],
+                    [89.71, 3476.00, 457.13],
+                    [-89.995, 1707.4, 426.62],
+                ]
+            )
+        )  # Last one is Sci on-axis for 21 Feb 23 - see Doc
+
+        # Pull out correct best-fit rotation and shift
+        th, Sx, Sz = (numpy.radians(bF[cam_idx, 0]), bF[cam_idx, 1], bF[cam_idx, 2])
+
+    else:
+        # Use supplied values
+        th, Sx, Sz = (numpy.radians(rot_shift[0]), rot_shift[1], rot_shift[2])
+
+    if verbose:
+        print("")
+        print("rot_shift_locs: rot_shift for ", camera, numpy.degrees(th), Sx, Sz)
+        print("")
+
+    # Rotation matrix
+    M = numpy.array(([numpy.cos(th), -numpy.sin(th)], [numpy.sin(th), numpy.cos(th)]))
+
+    # 2xnPts rotation offset
+    rOff = numpy.tile(numpy.array([[800.0, 550.0]]).transpose(), (1, in_locs.shape[0]))
+
+    # Make 2xnPts Sx,Sz offset matrix
+    off = numpy.tile(numpy.array([[Sx, Sz]]).transpose(), (1, in_locs.shape[0]))
+
+    # Shift to center, Rotate, shift back, and offset
+    rsLoc = numpy.dot(M, in_locs.T - rOff) + rOff + off
+
+    # Return calculated positions (need to transpose for standard layout)
+    return rsLoc.T
+
+
+def solve_locs(
+    locs: pandas.DataFrame,
+    ra: float,
+    dec: float,
+    full_frame=True,
+    verbose=False,
+):
+    """Use astrometry.net to solve field, given a series of star locations.
+
+    Parameters
+    ----------
+    locs
+        A Pandas DataFrame of ``x``, ``y``, and ``flux`` star locations.
+    ra
+        The estimated RA of the field.
+    dec
+        The estimated Dec of the field.
+    full_frame
+        Whether this is a full "master frame" field, requiring a different set of
+        index files.
+    verbose
+        Output additional information.
+
+    Returns
+    -------
+    myWCS
+        A WCS entity (or `None` if it fails) with appropriate location information.
+    solveInfo
+        A list containing information about the solve:
+        solveInfo[0] - [raSlv,deSlv]  RA,Dec of central pixel
+                 [1] - angSlv         Rotation of field
+                 [2] - pxScl          Arcsec / pixel of solve
+
+    """
+
+    lower_bound = 0.8  # Pixel scale hints (arcsec/pixel)
+    upper_bound = 1.2
+    radius = 1  # Search radius in degrees
+
+    if full_frame:
+        midX, midZ = 2500.0, 1000.0  # Middle of Master Frame
+        scales = [4, 5, 6, 7, 8]
+    else:
+        midX, midZ = 800.0, 550.0  # Middle of AG camera Frame
+        scales = [5, 6]
+
+    wcs = astrometrynet_quick(
+        "/data/astrometrynet/5200",
+        locs,
+        ra=ra,
+        dec=dec,
+        radius=radius,
+        pixel_scale=(upper_bound + lower_bound) / 2,
+        pixel_scale_factor_hi=upper_bound,
+        pixel_scale_factor_lo=lower_bound,
+        scales=scales,
+        series=5200,
+    )
+
+    if verbose:
+        print("")
+        print("Solution has match ? ", wcs is not None)
+
+    if wcs is None:
+        return (None, None)
+
+    # Calculate center and rotation of field
+
+    tM = wcs.wcs.cd  # Transformation matrix
+    angSlv = numpy.degrees(numpy.arctan2(tM[0, 0], tM[1, 0]))  # Angle from scaling
+
+    raSlv = pixel_to_skycoord(midX, midZ, wcs).ra
+    deSlv = pixel_to_skycoord(midX, midZ, wcs).dec
+
+    pxScl = numpy.abs(tM[0, 0] * 3600)
+
+    solveInfo = [[raSlv, deSlv], angSlv, pxScl]  # Solve information
+
+    if verbose:
+        print("Central pixel         :", midX, midZ)
+        print("RA central pixel      :", raSlv)
+        print("Dec central pixel     :", deSlv)
+        print("Field rotation (deg)  :", angSlv)
+        print("Pixel scale           :", pxScl)
+
+    return wcs, solveInfo
+
+
+def solve_from_files(files: list[str], telescope: str):
+    """Determines the telescope pointing from a set of AG frames.
+
+    Parameters
+    ----------
+    files
+        The AG FITS files to use determine the telescope pointing. Normally
+        these are a pair of east/west camera frames, except for ``spec``.
+    telescope
+        The telescope to which these exposures are associated.
+
+    Returns
+    -------
+    wcs
+        The wcs of the astrometric solution or `None` if no solution was found.
+
+    """
+
+    if len(files) == 0:
+        raise ValueError("No files provided.")
+
+    mf_sources: list[pandas.DataFrame] = []
+    ra = 0.0
+    dec = 0.0
+    for file in files:
+        header = fits.getheader(file)
+        camname = header["CAMNAME"][0].lower()
+        ra = header["RA"]
+        dec = header["DEC"]
+
+        sources = pandas.DataFrame(fits.getdata(file, "SOURCES"))
+        xy = sources[["x", "y"]].values
+
+        camera = f"{telescope}-{camname}"
+        file_locs = rot_shift_locs(camera, xy)
+        sources.loc[:, ["x", "y"]] = file_locs
+        mf_sources.append(sources)
+
+    locs = pandas.concat(mf_sources)
+    wcs, _ = solve_locs(locs[["x", "y", "flux"]], ra=ra, dec=dec, full_frame=True)
+
+    return wcs
