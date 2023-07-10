9d86724b034b45833b642f6f609c24c620a628f3
José Sánchez-Gallego <gallegoj@uw.edu>
2023-07-09 18:55:28 -0700

    Add missing from __future__ import annotations

--- src/lvmguider/transformations.py
+++ src/lvmguider/transformations.py
@@ -6,6 +6,8 @@
 # @Filename: transformations.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 
+from __future__ import annotations
+
 import pathlib
 import warnings
 from datetime import datetime
