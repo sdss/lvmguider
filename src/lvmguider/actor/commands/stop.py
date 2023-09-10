c9c7048189d2933e45fca6182bbe6553e3161eb2
José Sánchez-Gallego <gallegoj@uw.edu>
2023-09-10 17:20:44 +0000

    Add stop --soft

--- src/lvmguider/actor/commands/stop.py
+++ src/lvmguider/actor/commands/stop.py
@@ -13,6 +13,8 @@ from contextlib import suppress
 
 from typing import TYPE_CHECKING
 
+import click
+
 from lvmguider.actor import lvmguider_parser
 from lvmguider.maskbits import GuiderStatus
 
@@ -25,12 +27,20 @@ __all__ = ["stop"]
 
 
 @lvmguider_parser.command()
-async def stop(command: GuiderCommand):
+@click.option("--soft", is_flag=True, help="Stops the guider less aggressively.")
+async def stop(command: GuiderCommand, soft: bool = False):
     """Stops the guide loop."""
 
     if command.actor.guide_task is None or command.actor.guide_task.done():
         command.actor.status = GuiderStatus.IDLE
         return command.finish("Guider is not active.")
+
+    if soft:
+        if command.actor.status & GuiderStatus.STOPPING:
+            return command.fail("Guider loop is already stopping.")
+
+        command.actor.status |= GuiderStatus.STOPPING
+        return command.finish("Stopping the guide loop.")
 
     command.actor.guide_task.cancel()
     with suppress(asyncio.CancelledError):
