83d9fada812d3477c89214e135c18150b5d3188a
José Sánchez-Gallego <gallegoj@uw.edu>
2024-12-27 21:14:19 -0800

    Add some actual tests to make sure some parts of the code are touched

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,39 @@
 # @Date: 2023-01-17
 # @Filename: conftest.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+import pytest
+
+from clu.testing import setup_test_actor
+
+from lvmguider.actor.actor import LVMGuiderActor
+
+
+actor_config = {
+    "telescope": "sci",
+    "has_kmirror": True,
+    "guide_in_rot": True,
+    "focus": {
+        "model": {
+            "a": 0.1225,
+            "b": 34.435,
+        }
+    },
+    "actor": {
+        "name": "lvm.sci.guider",
+        "host": "10.8.38.21",
+        "port": 5672,
+    },
+}
+
+
+@pytest.fixture()
+async def actor():
+    _actor = LVMGuiderActor.from_config(actor_config)
+
+    _actor = await setup_test_actor(_actor)  # type: ignore
+
+    yield _actor
+
+    _actor.mock_replies.clear()
+    await _actor.stop()
--- tests/test_lvmguider.py
+++ tests/test_lvmguider.py
@@ -7,5 +7,22 @@
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 
 
-def test_placeholder():
-    assert True
+from pytest_mock import MockerFixture
+
+from lvmguider import config
+from lvmguider.actor.actor import LVMGuiderActor
+from lvmguider.guider import Guider
+
+
+def test_placeholder(mocker: MockerFixture):
+    command = mocker.MagicMock()
+    command.actor.config = config
+
+    guider = Guider(command, (0.0, 10.0, 0.0))
+
+    assert guider.command is not None
+
+
+async def test_actor(actor: LVMGuiderActor):
+    assert isinstance(actor, LVMGuiderActor)
+    assert actor.name == "lvm.sci.guider"
